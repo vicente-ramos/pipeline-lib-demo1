3907972be0232db5baf6f32e647cb4592267146c
Vicente Ramos <vramosga@gmail.com>
2020-08-08 17:49:45 -0300

    Read the report to string.

--- vars/coverage_upload_with_project.groovy
+++ vars/coverage_upload_with_project.groovy
@@ -7,7 +7,7 @@ def call(String projectName, String username , String language, String file) {
 	// def commitUuid = sh(returnStdout: true, script: "git log -n 1 --pretty=format:'%h'").trim()
 	def commitUuid = '578659cdd56e2dc511f13fde05b4969190cbaeb5'
 
-	File new_file = new File(file)
+	String new_file = new File(file).getText('UTF-8')
 
 	def cover_file = JsonOutput.toJson(new_file)
 	String url = "https://api.codacy.com/2.0/${username}/${projectName}/commit/${commitUuid}/coverage/${language}"
