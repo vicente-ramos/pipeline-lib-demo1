0a70c10c1b93ee94c7d304641fa3b092c2dd0e75
Vicente Ramos <vramosga@gmail.com>
2020-08-08 18:01:45 -0300

    Add debuging lines.

--- vars/coverage_upload_with_project.groovy
+++ vars/coverage_upload_with_project.groovy
@@ -7,8 +7,12 @@ def call(String projectName, String username , String language, String file) {
 	// def commitUuid = sh(returnStdout: true, script: "git log -n 1 --pretty=format:'%h'").trim()
 	def commitUuid = '578659cdd56e2dc511f13fde05b4969190cbaeb5'
 
+	println('Parte 1')
 	String new_file = new File(file).getText('UTF-8')
-
+	
+	println('the file was found and read')
+	println(new_file)
+	
 	def cover_file = JsonOutput.toJson(new_file)
 	String url = "https://api.codacy.com/2.0/${username}/${projectName}/commit/${commitUuid}/coverage/${language}"
 	
