33985bbd34484f865f758a5cbebf3d3eb7e55a79
Vicente Ramos <vramosga@gmail.com>
2020-08-08 18:48:37 -0300

    Try reading project details

--- vars/coverage_upload_with_project.groovy
+++ vars/coverage_upload_with_project.groovy
@@ -3,37 +3,52 @@ import java.net.HttpURLConnection;
 import groovy.json.*
 
 def call(String projectName, String username , String language, String file) {
+	// Get commitUuid
 	// def commitUuid = '578659cdd56e2dc511f13fde05b4969190cbaeb5'
 	def commitUuid = '578659c'
 	// def commitUuid = sh(returnStdout: true, script: 'git rev-parse HEAD').trim()
 	// def commitUuid = sh(returnStdout: true, script: "git log -n 1 --pretty=format:'%h'").trim()
 	
-	def actual_directory = sh(returnStdout: true, script: "pwd").trim()
-	def coverage_path = actual_directory + '/' + file
+	// Get URL
 
-	String report = new File(coverage_path).getText('UTF-8')
-	report = report.replace("\n", "").replace("\r", "").replace("\t", "");
-	
-	def cover_file = JsonOutput.toJson(report)
-	String url = "https://api.codacy.com/2.0/${username}/${projectName}/commit/${commitUuid}/coverage/${language}"
-	
-	println(url)
-	println(cover_file)
+	URL obj = new URL("https://api.codacy.com/2.0/${username}/${projectName}/commit/${commitUuid}");
+	HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+	con.setRequestMethod("GET");
+	int responseCode = con.getResponseCode();
 
-	URL new_url = new URL(url)
-	HttpURLConnection post = (HttpURLConnection) new_url.openConnection();
-	post.setRequestMethod("POST")
-	post.setDoOutput(true)
-	// post.setRequestProperty("project_token", token)
-	post.setRequestProperty("Content-Type", "application/json")
+	println('La respuesta para el get fue: ' + responseCode)
 
-	post.getOutputStream().write(cover_file.getBytes("UTF-8"))
-	
-	def postRC = post.getResponseCode()
+	return responseCode
 
-	if(postRC.equals(200)) {
-		println(post.getInputStream().getText())
-	}
+
+
+
+	// def actual_directory = sh(returnStdout: true, script: "pwd").trim()
+	// def coverage_path = actual_directory + '/' + file
+
+	// String report = new File(coverage_path).getText('UTF-8')
+	// report = report.replace("\n", "").replace("\r", "").replace("\t", "");
 	
-	return postRC
+	// def cover_file = JsonOutput.toJson(report)
+	// String url = "https://api.codacy.com/2.0/${username}/${projectName}/commit/${commitUuid}/coverage/${language}"
+	
+	// println(url)
+	// println(cover_file)
+
+	// URL new_url = new URL(url)
+	// HttpURLConnection post = (HttpURLConnection) new_url.openConnection();
+	// post.setRequestMethod("POST")
+	// post.setDoOutput(true)
+	// // post.setRequestProperty("project_token", token)
+	// post.setRequestProperty("Content-Type", "application/json")
+
+	// post.getOutputStream().write(cover_file.getBytes("UTF-8"))
+	
+	// def postRC = post.getResponseCode()
+
+	// if(postRC.equals(200)) {
+	// 	println(post.getInputStream().getText())
+	// }
+	
+	// return postRC
 }
